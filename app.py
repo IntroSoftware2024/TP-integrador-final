c2b71808bb63ff787119628ede6dea5cb92ef40c
agustin <agustinneme7@gmail.com>
2024-06-02 18:30:02 -0300

    Agrego base para el desarrollo de la api

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import create_engine
+from sqlalchemy import text
+from sqlalchemy.exc import SQLAlchemyError
+
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def hola_mundo():
+    return 
+
+
+if __name__ == '__main__':
+    app.run('127.0.0.1', port='8080', debug=True)
