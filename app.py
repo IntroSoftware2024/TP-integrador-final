69a9e726ad4147f9ddc912eb4db1e8b562aab6d5
chiaraLopezAn <chiara1991.cl@gmail.com>
2024-06-04 20:09:25 -0300

    Creo una función para mostrar un emprendimiento en especifico.

--- app.py
+++ app.py
@@ -8,6 +8,31 @@ app = Flask(__name__)
 
 engine = create_engine("mysql+mysqlconnector://root@localhost/emprende_facil")
 
+@app.route('/emprendimientos/<id>', methods = ['GET'])
+def mostrar_emprendimiento(id):
+    conn = engine.connect()
+    query = f"""SELECT emprendimiento_id, nombre, descripcion, categoria, direccion, contacto
+            FROM emprendimientos
+            WHERE emprendimiento_id = {id};
+            """
+    try:
+        result = conn.execute(text(query))
+        conn.commit()
+        conn.close()
+    except SQLAlchemyError as err:
+        return jsonify(str(err.__cause__))
+    if result.rowcount !=0:
+        data = {}
+        row = result.first()
+        data['id'] = row[0]
+        data['nombre'] = row[1]
+        data['descripcion'] = row[2]
+        data['categoria'] = row[3]
+        data['direccion'] = row[4]
+        data['contacto'] = row[5]
+        return jsonify(data), 200
+    return jsonify({"message": "El emprendimiento buscado no existe"}), 404
+
 
 if __name__ == '__main__':
     app.run('127.0.0.1', port='8080', debug=True)
