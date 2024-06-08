03125715d9d0b22223314475d3d4376744f837a6
Geronimo Medina <gmedinam@fi.uba.ar>
2024-06-08 20:14:12 -0300

    Agrego las funciones del mapa

--- app.py
+++ app.py
@@ -1,189 +1,245 @@
-from flask import Flask, request, jsonify
+from flask import Flask, jsonify, request, abort
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 from sqlalchemy.exc import SQLAlchemyError
+from math import radians, cos, sin, asin, sqrt
+import requests
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = "Uri de la base" # Agregar la URI de la base de datos
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///emprendimientos.db'
 db = SQLAlchemy(app)
-
-# ---- Modelos ----
+migrate = Migrate(app, db)
 
 class Emprendimiento(db.Model):
-    __tablename__ = 'emprendimientos'
-    emprendimiento_id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     nombre = db.Column(db.String(100), nullable=False)
     descripcion = db.Column(db.Text)
-    categoria = db.Column(db.String(50))
+    categoria = db.Column(db.String(100))
     direccion = db.Column(db.String(200))
-    latitud = db.Column(db.Float)
-    longitud = db.Column(db.Float)
-    contacto = db.Column(db.String(50))
-    usuario_id = db.Column(db.Integer, db.ForeignKey('usuarios.usuarios_id'), nullable=True)
-    usuario = db.relationship('Usuario', backref=db.backref('emprendimientos', lazy=True))
+    contacto = db.Column(db.String(100))
+    usuario_id = db.Column(db.Integer, db.ForeignKey('usuario.id'), nullable=True)
 
 class Usuario(db.Model):
-    __tablename__ = 'usuarios'
-    usuarios_id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     nombre = db.Column(db.String(100), nullable=False)
-    email = db.Column(db.String(100), nullable=False, unique=True)
+    email = db.Column(db.String(100), unique=True, nullable=False)
     contraseña = db.Column(db.String(255), nullable=False)
+    emprendimientos = db.relationship('Emprendimiento', backref='usuario', lazy=True)
 
-# ---- Rutas de Emprendimientos ---- 
-
-# Endpoint para agregar emprendimientos
-@app.route('/emprendimientos', methods=['POST'])
-def agregar_emprendimiento():
-    data = request.json
-    try:
-        nuevo_emprendimiento = Emprendimiento(**data)
-        db.session.add(nuevo_emprendimiento)
-        db.session.commit()
-        return jsonify({'message': 'Emprendimiento agregado exitosamente'}), 201
-    except SQLAlchemyError as e:
-        db.session.rollback()
-        return jsonify({'message': 'Error al agregar emprendimiento', 'error': str(e)}), 400
-
-# Endpoint para listar todos los emprendimientos
 @app.route('/emprendimientos', methods=['GET'])
 def listar_emprendimientos():
     emprendimientos = Emprendimiento.query.all()
-    output = []
-    for emprendimiento in emprendimientos:
-        emprendimiento_data = {
-            'emprendimiento_id': emprendimiento.emprendimiento_id,
-            'nombre': emprendimiento.nombre,
-            'descripcion': emprendimiento.descripcion,
-            'categoria': emprendimiento.categoria,
-            'direccion': emprendimiento.direccion,
-            'latitud': emprendimiento.latitud,
-            'longitud': emprendimiento.longitud,
-            'contacto': emprendimiento.contacto,
-            'usuario_id': emprendimiento.usuario_id
-        }
-        output.append(emprendimiento_data)
-    return jsonify({'emprendimientos': output}), 200
+    emprendimientos_json = [{'id': emprendimiento.id, 'nombre': emprendimiento.nombre, 'descripcion': emprendimiento.descripcion, 'categoria': emprendimiento.categoria, 'direccion': emprendimiento.direccion, 'contacto': emprendimiento.contacto, 'usuario_id': emprendimiento.usuario_id} for emprendimiento in emprendimientos]
+    return jsonify(emprendimientos_json)
 
-# Endpoint para mostrar un emprendimiento específico por su ID
+@app.route('/emprendimientos', methods=['POST'])
+def agregar_emprendimiento():
+    if not request.json or not 'nombre' in request.json:
+        abort(400)
+    nuevo_emprendimiento = Emprendimiento(
+        nombre=request.json['nombre'],
+        descripcion=request.json.get('descripcion', ""),
+        categoria=request.json.get('categoria', ""),
+        direccion=request.json.get('direccion', ""),
+        contacto=request.json.get('contacto', ""),
+        usuario_id=request.json.get('usuario_id', None)
+    )
+    try:
+        db.session.add(nuevo_emprendimiento)
+        db.session.commit()
+    except SQLAlchemyError as err:
+        db.session.rollback()
+        return jsonify({'message': str(err.__cause__)}), 500
+    return jsonify({'id': nuevo_emprendimiento.id}), 201
+
 @app.route('/emprendimientos/<int:id>', methods=['GET'])
-def mostrar_emprendimiento(id):
-    emprendimiento = Emprendimiento.query.get(id)
-    if not emprendimiento:
-        return jsonify({'message': 'Emprendimiento no encontrado'}), 404
-    emprendimiento_data = {
-        'emprendimiento_id': emprendimiento.emprendimiento_id,
+def obtener_emprendimiento(id):
+    emprendimiento = Emprendimiento.query.get_or_404(id)
+    data = {
+        'id': emprendimiento.id,
         'nombre': emprendimiento.nombre,
         'descripcion': emprendimiento.descripcion,
         'categoria': emprendimiento.categoria,
         'direccion': emprendimiento.direccion,
-        'latitud': emprendimiento.latitud,
-        'longitud': emprendimiento.longitud,
         'contacto': emprendimiento.contacto,
         'usuario_id': emprendimiento.usuario_id
     }
-    return jsonify(emprendimiento_data), 200
+    return jsonify(data), 200
 
-# Endpoint para actualizar un emprendimiento por su ID
 @app.route('/emprendimientos/<int:id>', methods=['PUT'])
 def actualizar_emprendimiento(id):
-    emprendimiento = Emprendimiento.query.get(id)
-    if not emprendimiento:
-        return jsonify({'message': 'Emprendimiento no encontrado'}), 404
-    data = request.json
+    emprendimiento = Emprendimiento.query.get_or_404(id)
+    if not request.json:
+        abort(400)
+    nombre = request.json.get('nombre', emprendimiento.nombre)
+    descripcion = request.json.get('descripcion', emprendimiento.descripcion)
+    categoria = request.json.get('categoria', emprendimiento.categoria)
+    direccion = request.json.get('direccion', emprendimiento.direccion)
+    contacto = request.json.get('contacto', emprendimiento.contacto)
+    usuario_id = request.json.get('usuario_id', emprendimiento.usuario_id)
+    
+    emprendimiento.nombre = nombre
+    emprendimiento.descripcion = descripcion
+    emprendimiento.categoria = categoria
+    emprendimiento.direccion = direccion
+    emprendimiento.contacto = contacto
+    emprendimiento.usuario_id = usuario_id
+    
     try:
-        for key, value in data.items():
-            setattr(emprendimiento, key, value)
         db.session.commit()
-        return jsonify({'message': 'Emprendimiento actualizado exitosamente'}), 200
-    except SQLAlchemyError as e:
+    except SQLAlchemyError as err:
         db.session.rollback()
-        return jsonify({'message': 'Error al actualizar emprendimiento', 'error': str(e)}), 400
+        return jsonify({'message': str(err.__cause__)}), 500
+    
+    return jsonify({'id': emprendimiento.id, 'nombre': emprendimiento.nombre, 'descripcion': emprendimiento.descripcion, 'categoria': emprendimiento.categoria, 'direccion': emprendimiento.direccion, 'contacto': emprendimiento.contacto, 'usuario_id': emprendimiento.usuario_id})
 
-# Endpoint para eliminar un emprendimiento por su ID
 @app.route('/emprendimientos/<int:id>', methods=['DELETE'])
 def eliminar_emprendimiento(id):
-    emprendimiento = Emprendimiento.query.get(id)
-    if not emprendimiento:
-        return jsonify({'message': 'Emprendimiento no encontrado'}), 404
+    emprendimiento = Emprendimiento.query.get_or_404(id)
     try:
         db.session.delete(emprendimiento)
         db.session.commit()
-        return jsonify({'message': 'Emprendimiento eliminado exitosamente'}), 202
-    except SQLAlchemyError as e:
+    except SQLAlchemyError as err:
         db.session.rollback()
-        return jsonify({'message': 'Error al eliminar emprendimiento', 'error': str(e)}), 400
+        return jsonify({'message': str(err.__cause__)}), 500
+    
+    return jsonify({'resultado': True})
 
-# ---- Rutas de Usuarios ---- 
-
-# Endpoint para agregar usuarios
-@app.route('/usuarios', methods=['POST'])
-def agregar_usuario():
-    data = request.json
-    try:
-        nuevo_usuario = Usuario(**data)
-        db.session.add(nuevo_usuario)
-        db.session.commit()
-        return jsonify({'message': 'Usuario agregado exitosamente'}), 201
-    except SQLAlchemyError as e:
-        db.session.rollback()
-        return jsonify({'message': 'Error al agregar usuario', 'error': str(e)}), 400
-
-# Endpoint para listar todos los usuarios
 @app.route('/usuarios', methods=['GET'])
 def listar_usuarios():
     usuarios = Usuario.query.all()
-    output = []
-    for usuario in usuarios:
-        usuario_data = {
-            'usuarios_id': usuario.usuarios_id,
-            'nombre': usuario.nombre,
-            'email': usuario.email
-        }
-        output.append(usuario_data)
-    return jsonify({'usuarios': output}), 200
+    usuarios_json = [{'id': usuario.id, 'nombre': usuario.nombre, 'email': usuario.email} for usuario in usuarios]
+    return jsonify(usuarios_json)
 
-# Endpoint para mostrar un usuario específico por su ID
+@app.route('/usuarios', methods=['POST'])
+def agregar_usuario():
+    if not request.json or not 'nombre' in request.json or not 'email' in request.json or not 'contraseña' in request.json:
+        abort(400)
+    nuevo_usuario = Usuario(
+        nombre=request.json['nombre'],
+        email=request.json['email'],
+        contraseña=request.json['contraseña']
+    )
+    try:
+        db.session.add(nuevo_usuario)
+        db.session.commit()
+    except SQLAlchemyError as err:
+        db.session.rollback()
+        return jsonify({'message': str(err.__cause__)}), 500
+    return jsonify({'id': nuevo_usuario.id}), 201
+
 @app.route('/usuarios/<int:id>', methods=['GET'])
-def mostrar_usuario(id):
-    usuario = Usuario.query.get(id)
-    if not usuario:
-        return jsonify({'message': 'Usuario no encontrado'}), 404
-    usuario_data = {
-        'usuarios_id': usuario.usuarios_id,
+def obtener_usuario(id):
+    usuario = Usuario.query.get_or_404(id)
+    data = {
+        'id': usuario.id,
         'nombre': usuario.nombre,
         'email': usuario.email
     }
-    return jsonify(usuario_data), 200
+    return jsonify(data), 200
 
-# Endpoint para actualizar un usuario por su ID
 @app.route('/usuarios/<int:id>', methods=['PUT'])
 def actualizar_usuario(id):
-    usuario = Usuario.query.get(id)
-    if not usuario:
-        return jsonify({'message': 'Usuario no encontrado'}), 404
-    data = request.json
+    usuario = Usuario.query.get_or_404(id)
+    if not request.json:
+        abort(400)
+    nombre = request.json.get('nombre', usuario.nombre)
+    email = request.json.get('email', usuario.email)
+    contraseña = request.json.get('contraseña', usuario.contraseña)
+    
+    usuario.nombre = nombre
+    usuario.email = email
+    usuario.contraseña = contraseña
+    
     try:
-        for key, value in data.items():
-            setattr(usuario, key, value)
         db.session.commit()
-        return jsonify({'message': 'Usuario actualizado exitosamente'}), 200
-    except SQLAlchemyError as e:
+    except SQLAlchemyError as err:
         db.session.rollback()
-        return jsonify({'message': 'Error al actualizar usuario', 'error': str(e)}), 400
+        return jsonify({'message': str(err.__cause__)}), 500
+    
+    return jsonify({'id': usuario.id, 'nombre': usuario.nombre, 'email': usuario.email})
 
-# Endpoint para eliminar un usuario por su ID
 @app.route('/usuarios/<int:id>', methods=['DELETE'])
 def eliminar_usuario(id):
-    usuario = Usuario.query.get(id)
-    if not usuario:
-        return jsonify({'message': 'Usuario no encontrado'}), 404
+    usuario = Usuario.query.get_or_404(id)
     try:
         db.session.delete(usuario)
         db.session.commit()
-        return jsonify({'message': 'Usuario eliminado exitosamente'}), 202
-    except SQLAlchemyError as e:
+    except SQLAlchemyError as err:
         db.session.rollback()
-        return jsonify({'message': 'Error al eliminar usuario', 'error': str(e)}), 400
+        return jsonify({'message': str(err.__cause__)}), 500
+    
+    return jsonify({'resultado': True})
 
+# Función para obtener coordenadas utilizando la API de OpenCage
+def obtener_coordenadas(direccion):
+    llave_api = 'aca iria la api'
+    url = f'https://api.opencagedata.com/geocode/v1/json?q={direccion}&{llave_api}'
+    
+    respuesta = requests.get(url)
+    datos = respuesta.json()
+    
+    if datos['results']:
+        lat = datos['results'][0]['geometry']['lat']
+        lon = datos['results'][0]['geometry']['lng']
+        return (lat, lon)
+    else:
+        return None
+
+# Función  para calcular la distancia entre dos puntos geográficos
+def distancia(lat1, lon1, lat2, lon2):
+    # Convertir grados a radianes
+    lat1, lon1, lat2, lon2 = map(radians, [lat1, lon1, lat2, lon2])
+    
+    diferencia_longitud = lon2 - lon1
+    diferencia_latitud = lat2 - lat1
+    
+    # Aplicar fórmula de haversine
+    a = sin(diferencia_latitud/2)**2 + cos(lat1) * cos(lat2) * sin(diferencia_longitud/2)**2
+    c = 2 * asin(sqrt(a))
+    
+    radio_tierra = 6371
+    
+    return c * radio_tierra
+
+# Ruta para buscar emprendimientos cercanos a una dirección
+@app.route('/buscar', methods=['GET'])
+def buscar_emprendimientos_cercanos():
+    direccion_usuario = request.args.get('direccion')
+    
+    # Obtener las coordenadas de la dirección del usuario
+    coordenadas_usuario = obtener_coordenadas(direccion_usuario)
+    
+    if not coordenadas_usuario:
+        return jsonify({'mensaje': 'No se pudo encontrar la dirección proporcionada.'}), 400
+    
+    lat_usuario, lon_usuario = coordenadas_usuario
+    distancia_maxima = 5  # Distancia máxima en kilómetros
+    
+    emprendimientos = Emprendimiento.query.all()
+    emprendimientos_cercanos = []
+    
+    for emprendimiento in emprendimientos:
+        # Obtener las coordenadas de la dirección del emprendimiento
+        coordenadas_emprendimiento = obtener_coordenadas(emprendimiento.direccion)
+        
+        if coordenadas_emprendimiento:
+            lat_emp, lon_emp = coordenadas_emprendimiento
+            distancia = distancia(lat_usuario, lon_usuario, lat_emp, lon_emp)
+            
+            if distancia <= distancia_maxima:
+                emprendimientos_cercanos.append({
+                    'id': emprendimiento.id,
+                    'nombre': emprendimiento.nombre,
+                    'descripcion': emprendimiento.descripcion,
+                    'categoria': emprendimiento.categoria,
+                    'direccion': emprendimiento.direccion,
+                    'contacto': emprendimiento.contacto,
+                    'usuario_id': emprendimiento.usuario_id,
+                    'distancia': distancia
+                })
+    
+    return jsonify(emprendimientos_cercanos), 200
 
 if __name__ == '__main__':
-    app.run('127.0.0.1', port='8080', debug=True)
+    app.run('127.0.0.1', port=8080, debug=True)
