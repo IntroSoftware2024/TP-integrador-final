b885eb3309421dd60a249c1edfb653f5876104c7
Nicolás Mutazzi <nmutazzi@fi.uba.ar>
2024-06-09 17:47:13 -0300

    Se actualiza el app.py para que index sea la ruta inicial.

--- app.py
+++ app.py
@@ -3,10 +3,6 @@ from flask import Flask, render_template
 app = Flask("__main__")
 
 @app.route("/")
-def base():
-    return render_template('base.html')
-
-@app.route("/index")
 def index():
     return render_template('index.html')
 
