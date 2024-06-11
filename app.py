5955c29a50202ce25e6a6bf53302ea416e3b1f5b
chiaraLopezAn <chiara1991.cl@gmail.com>
2024-06-11 18:59:12 -0300

    1 - Front: actualizo el app.py

--- app.py
+++ app.py
@@ -23,38 +23,5 @@ def page_not_found(error):
     return render_template('error404.html'), 404
 
 
-@app.route('/form', methods=['GET', 'POST'])
-def registrar_emp():
-    if request.method == 'POST':
-        nombre = request.form['nombre']
-        instagram = request.form['instagram']
-        localidad = request.form['localidad']
-        provincia = request.form['provincia']
-        contacto = request.form['contacto']
-        categoria  = request.form['categoria']
-        descripcion = request.form['descripcion']
-
-        '''    
-        conexion_MySQLdb = connectionBD()
-        cursor = conexion_MySQLdb.cursor(dictionary=True)
-            
-        sql = ("INSERT INTO empleados(nombre, instagram, localidad, provincia, contacto, categoria, descripcion) VALUES (%s, %s, %s, %s)")
-        valores = (nombre, instagram, localidad, provincia, contacto, categoria, descripcion)
-        cursor.execute(sql, valores)
-        conexion_MySQLdb.commit()
-        
-        cursor.close() #Cerrando conexion SQL
-        conexion_MySQLdb.close() #cerrando conexion de la BD
-        msg = 'Registro con exito'
-        
-        print(cursor.rowcount, "registro insertado")
-        print("1 registro insertado, id", cursor.lastrowid)
-        '''
-
-        return render_template('subir_emp.html', msg='Formulario enviado')
-    else:
-        return render_template('subir_emp.html', msg = 'Metodo HTTP incorrecto')
-
-
 if __name__ == '__main__':
     app.run('127.0.0.1', port='8080', debug=True)
