0f9a6a2aece38edc49304e614da180e242162b59
Agustin Neme <nemeagustin78@gmail.com>
2024-06-17 15:10:20 -0300

    Agregro funcionalidad para listar emprendimientos en front

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Blueprint, render_template, request
 
 app = Blueprint('app', __name__)
 
+API_URL = 'http://127.0.0.1:5000/emprendimientos'
+
 @app.route("/")
 def index():
     return render_template('index.html')
@@ -13,12 +15,23 @@ def emprendimientos(categoria):
 
     palabra = request.args.get('palabra', '')
     provincia = request.args.get('provincia', '')
+    try:
+        response = request.get(API_URL)
+        response.raise_for_status()
+        emprendimientos = response.json().get('emprendimientos', [])
+    except request.exceptions.RequestException as e:
+        print(f"Error fetching data: {e}")
+        emprendimientos = []
 
-    if not palabra and not provincia:
-        return render_template('emprendimientos.html', categoria=categoria)
+    # Filtrar los emprendimientos según la categoría, palabra y provincia
+    if palabra:
+        emprendimientos = [e for e in emprendimientos if palabra.lower() in e['nombre'].lower() or palabra.lower() in e['descripcion'].lower()]
+    if provincia:
+        emprendimientos = [e for e in emprendimientos if provincia.lower() in e['provincia'].lower()]
+    if categoria != 'busqueda':
+        emprendimientos = [e for e in emprendimientos if categoria.lower() in e['categoria'].lower()]
 
-    return render_template('emprendimientos.html', categoria=categoria, palabra=palabra, provincia=provincia)
-
+    return render_template('emprendimientos.html', categoria=categoria, emprendimientos=emprendimientos, palabra=palabra, provincia=provincia)
 @app.route("/subir_emp")
 def subir_emp():
     return render_template('subir_emp.html')
